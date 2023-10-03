0ee6766158e5d9786cfb3d29ad0b95c2f92a864f
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-03 23:55:00 +0300

    add Example 005 Condition If Else

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ira")
+{
+    Console.WriteLine("Wow! It`s Ira!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
